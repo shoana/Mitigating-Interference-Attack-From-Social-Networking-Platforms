eeffb40f4ba56da7e7fa3296c69f1165b3989a7c
Mounica Pillarisetty <35469706+mounicap6@users.noreply.github.com>
2020-12-04 12:24:54 -0500

    sql data for Mounica's FB data

---
+++ Database/Mounica/primary_location.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS primary_location (
+    `primary_location_city_region_pairs` VARCHAR(7) CHARACTER SET utf8,
+    `primary_location_zipcode` VARCHAR(3) CHARACTER SET utf8
+);
+INSERT INTO primary_location VALUES
+    ('Ottawa',NULL),
+    ('Ontario','K1S');
