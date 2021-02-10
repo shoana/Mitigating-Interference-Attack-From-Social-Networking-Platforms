7453a84cc62f90ad1a669f54fa45d66defd840ff
Mounica Pillarisetty <35469706+mounicap6@users.noreply.github.com>
2021-02-09 23:40:34 -0500

    Update extractNouns.py

--- extractNouns.py
+++ extractNouns.py
@@ -1,24 +1,30 @@
+import nltk
 import json
 import pandas as pd
 from pandas.io.json import json_normalize
-import nltk
 from nltk.tag import pos_tag
 from nltk import word_tokenize
 
-#create INSTAGRAM message data table
 with open('Data/message_1.json') as json_data:
   data = json.load(json_data)
 df = pd.DataFrame.from_dict(data['messages'])
+
+#extracting just the message content
 df3 = pd.DataFrame.from_dict(df['content'])
 
-print("--------------content--------------\n")
-print(df3)
+#stores the message data in a text file so it can be parsed with nltk to find nouns
+with open('messageData.txt', 'w') as f:
+    f.write(
+        df3.to_string(header = False, index = False)
+    )  
+file = open("messageData.txt", "r")
+lines = file.read()
 
-print("--------------nouns---------------\n")
+# function to test if something is a noun
+is_noun = lambda pos: pos[:2] == 'NN'
 
-#extracting nouns 
-df3['noun'] = df3.apply(lambda row: nltk.word_tokenize(row['noun']), axis=1) 
-noun=[]
-for  index, row in df3.iterrows():
-    noun.append([word for word,pos in pos_tag(row[0]) if pos == 'NN'])
-print(noun)
+# do the nlp stuff
+tokenized = nltk.word_tokenize(lines)
+nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)] 
+
+print (nouns)
